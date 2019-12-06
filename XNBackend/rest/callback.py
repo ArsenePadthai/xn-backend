61ece9da6c548c02728cdc3fdf9ebad5eff595e3
pedro <arsene.wang@huangloong.com>
2019-12-06 09:43:37 +0800

    [minor] update callback.py and add logfile settings to xn-sensor

--- XNBackend/rest/callback.py
+++ XNBackend/rest/callback.py
@@ -7,12 +7,14 @@ import pprint
 class AcsCallback(Resource):
     def post(self):
         json_body = request.get_json()
-        event = json_body['params']["events"][0]
-        event_type = event['eventType']
+        print('============================')
+        print(json_body)
+        # event = json_body['params']["events"][0]
+        # event_type = event['eventType']
 
-        if event_type == '196893':
-            face_recognition.delay(event['data']['ExtEventCardNo'], event['srcName'], event['eventId'])
-        elif event_type == '198919':
-            door_control.delay(event['srcName'], event['eventId'])
-        elif event_type == '198657':
-            door_destory.delay(event['srcName'], event['eventId'])
+        # if event_type == '196893':
+        #     face_recognition.delay(event['data']['ExtEventCardNo'], event['srcName'], event['eventId'])
+        # elif event_type == '198919':
+        #     door_control.delay(event['srcName'], event['eventId'])
+        # elif event_type == '198657':
+        #     door_destory.delay(event['srcName'], event['eventId'])
