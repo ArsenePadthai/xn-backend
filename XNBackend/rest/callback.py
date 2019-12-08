eacd35a8047c6a99fc95afb4804a373602cbde4a
pedro <arsene.wang@huangloong.com>
2019-12-08 17:27:41 +0800

    add handling of face callback

--- XNBackend/rest/callback.py
+++ XNBackend/rest/callback.py
@@ -1,26 +1,48 @@
 import logging
+from datetime import datetime
 from flask_restful import Resource
 from flask import request
-from XNBackend.task.hik.acs import * 
-import pprint
+from XNBackend.models import AppearRecords, db
 
 L = logging.getLogger()
-
 
 
 class AcsCallback(Resource):
     def post(self):
         json_body = request.get_json()
-        print('============================')
         print(json_body)
-        L.error(json_body)
-        return {}
-        # event = json_body['params']["events"][0]
-        # event_type = event['eventType']
+        ability = json_body['params']['ability']
+        if ability == 'event_face_recognition':
+            events = json_body['params']['events']
+            for e in events:
+                if e['eventType'] != 1644175361:
+                    L.info({f'not recongnize events'})
+                    return
+                else:
+                    happenTime = datetime.strptime(e['happenTime'][:19], '%Y-%m-%dT%H:%M:%S')
+                    resInfo = e['data']['resInfo'][0]
+                    deviceName = resInfo['cn']
+                    cameraIndexCode = resInfo['indexCode']
 
-        # if event_type == '196893':
-        #     face_recognition.delay(event['data']['ExtEventCardNo'], event['srcName'], event['eventId'])
-        # elif event_type == '198919':
-        #     door_control.delay(event['srcName'], event['eventId'])
-        # elif event_type == '198657':
-        #     door_destory.delay(event['srcName'], event['eventId'])
+                    snap = e['data']['faceRecognitionResult']['snap']
+                    facePicture = snap['faceUrl']
+
+                    faces = e['data']['faceRecognitionResult']['faceMatch']
+                    for f in faces:
+                        name = f['faceInfoName']
+                        sex = f['faceInfoSex']
+                        certificateNum = f['certificate']
+
+                        ap_record = AppearRecords(
+                            name=name,
+                            sex=sex,
+                            certificateNum=certificateNum,
+                            facePicture=facePicture,
+                            cameraIndexCode=cameraIndexCode,
+                            deviceName=deviceName,
+                            eventType=e['eventType'],
+                            happenTime=happenTime
+                        )
+                        db.session.add(ap_record)
+                        db.session.commit()
+        return {"ret": 'ok'}
