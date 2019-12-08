86b0d0987a5b23c3da2813644a50796032ee2e18
pedro <arsene.wang@huangloong.com>
2019-12-08 21:54:33 +0800

    add access door query feature

--- XNBackend/rest/acs.py
+++ XNBackend/rest/acs.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource, reqparse
 from XNBackend.task.hik.acs import *
 from XNBackend.task.hik.task import acs_control 
-from XNBackend.models.models import db, TrackingDevices, Locators
+from XNBackend.models.models import db, TrackingDevices, Locators, Door
 
 acs_parser = reqparse.RequestParser()
 acs_parser.add_argument('room_no', required=True, type=str)
@@ -29,37 +29,20 @@ class AcsControl(Resource):
 
 class Acs(Resource):
     def get(self):
-        def calc_acs(entries):
-            count_open = 0
-            count_infra = 0
-            for e in entries:
-                if e.acs_record:
-                    if e.acs_record.status == 1:
-                        count_open += 1
-                    if e.acs_record.event_type == 198657:
-                        count_infra += 1
-            return {
-                'open': count_open,
-                'infra': count_infra
-            }
-
         args = acs_get_parser.parse_args()
-        floor = args['floor']
-        acs = TrackingDevices.query.filter(
-            TrackingDevices.device_type == 1
-        ).filter(
-            TrackingDevices.locator_body.has(floor=floor)
-        ).all()
-        ret = {'total': len(acs)}
-        detail = dict()
-        for a in acs:
-            if not detail.get(a.locator_body.internal_code):
-                if a.acs_record:
-                    detail[a.locator_body.internal_code] = a.acs_record.status == 1
-
-        ret.update({"detail": detail})
-        ret.update(calc_acs(acs))
-        return ret
+        floor = args.get('floor')
+        doors = Door.query.filter(Door.room_no_internal.like(floor+'%'))
+        total_count = doors.count()
+        total_open = doors.filter(Door.status == 1)
+        total_open_count = total_open.count()
+        status_dict = dict()
+        for d in doors:
+            status_dict[d.room_no_internal] = d.status
+        return {
+            'total': total_count,
+            'open': total_open_count,
+            'detail': status_dict
+        }
 
 
 class AcsEvent(Resource):
--- XNBackend/rest/callback.py
+++ XNBackend/rest/callback.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 from flask_restful import Resource
 from flask import request
-from XNBackend.models import AppearRecords, db
+from XNBackend.models import AppearRecords, Door, db
 
 L = logging.getLogger()
 
@@ -10,7 +10,6 @@ L = logging.getLogger()
 class AcsCallback(Resource):
     def post(self):
         json_body = request.get_json()
-        print(json_body)
         ability = json_body['params']['ability']
         if ability == 'event_face_recognition':
             events = json_body['params']['events']
@@ -45,4 +44,24 @@ class AcsCallback(Resource):
                         )
                         db.session.add(ap_record)
                         db.session.commit()
-        return {"ret": 'ok'}
+
+        elif ability == 'event_acs':
+            events = json_body['params']['events']
+            for e in events:
+                event_type = e['eventType']
+                happenTime = datetime.strptime(e['happenTime'][:19], '%Y-%m-%dT%H:%M:%S')
+                if event_type == 199941:
+                    door_new_status = 1
+                elif event_type == 199942:
+                    door_new_status = 0
+                else:
+                    return
+                door_index_code = e['srcIndex']
+                door = Door.query.filter(Door.door_index_code == door_index_code).first()
+                if door:
+                    door.status = door_new_status
+                    door.updated_at = happenTime
+                    db.session.commit()
+                else:
+                    continue
+            return {"ret": 'ok'}
