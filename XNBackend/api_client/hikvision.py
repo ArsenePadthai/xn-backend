33b5050261d7ad20a642a6b00002d40d5ec71005
pedro <arsene.wang@huangloong.com>
2020-01-04 14:41:04 +0800

    add hikvision api_client

---
+++ XNBackend/api_client/hikvision.py
@@ -0,0 +1,65 @@
+import requests
+import hmac
+import json
+import base64
+import hashlib
+from datetime import datetime
+from flask import current_app
+
+
+def get_date_string():
+    now = datetime.now()
+    return now.strftime('%a %b %d %H:%M:%S' + " CST " + "%Y")
+
+
+def open_door(door_index_code, control_type=2):
+    """
+    Control the door to open. Closing is not supported.
+
+    Args:
+        door_index_code (str): device_index_code of the door in database
+        control_type: 2, without question
+    """
+    if control_type == 3:
+        return {"code": -1,
+                "message": "control type 3 is not allowed"}
+    req_body = {
+        "doorIndexCodes": [door_index_code],
+        "controlType": control_type
+    }
+
+    method = "POST"
+    accept = "*/*"
+    content_md5 = base64.b64encode(hashlib.md5(json.dumps(req_body).encode("utf-8")).digest()).decode()
+    date_string = get_date_string()
+    content_type = "application/json"
+    x_ca_signature_headers = "x-ca-key"
+    x_ca_key = current_app.config['HIK_APP_KEY']
+    path = "/artemis/api/acs/v1/door/doControl"
+
+    http_headers = method +"\n" + accept + "\n" + content_md5 +"\n" + content_type + "\n" + date_string + "\n"
+    custom_headers = x_ca_signature_headers + ":" + x_ca_key + "\n"
+    url = path
+    pre_sign = http_headers + custom_headers + url
+    print(pre_sign)
+
+    signed_str = base64.b64encode(hmac.new(bytes(current_app.config['HIK_APP_SECRET'], encoding='utf-8'),
+                                           bytes(pre_sign, encoding='utf-8'),
+                                           hashlib.sha256).digest()).decode('utf-8')
+
+    headers = {
+        "Accept": "*/*",
+        "Content-MD5": content_md5,
+        "Content-Type": "application/json",
+        "Date": get_date_string(),
+        "X-Ca-Key": current_app.config['HIK_APP_KEY'],
+        "X-Ca-Signature": signed_str,
+        "X-Ca-Signature-Headers": 'x-ca-key',
+    }
+
+    resp = requests.post('http://10.100.103.1/' + path, headers=headers, json=req_body)
+    j_resp = resp.json()
+    return {
+        "code": j_resp['code'],
+        "message": j_resp['msg']
+    }
--- XNBackend/rest/acs.py
+++ XNBackend/rest/acs.py
@@ -2,9 +2,11 @@ from flask_jwt_extended import jwt_required
 from flask_restful import Resource, reqparse
 from XNBackend.tasks.hik.tasks import acs_control
 from XNBackend.models.models import TrackingDevices, Door
+from XNBackend.api_client.hikvision import open_door
 
 acs_parser = reqparse.RequestParser()
-acs_parser.add_argument('room_no', required=True, type=str)
+acs_parser.add_argument('room_no', required=False, type=str)
+acs_parser.add_argument('device_index_code', required=False, type=str)
 
 acs_get_parser = reqparse.RequestParser()
 acs_get_parser.add_argument('floor',
@@ -17,15 +19,35 @@ class AcsControl(Resource):
     @jwt_required
     def patch(self):
         args = acs_parser.parse_args()
-        room_no = int(args.get('room_no'))
-
-        device = TrackingDevices.query.filter(
-            TrackingDevices.locator_body.has(zone=room_no)
-        ).first()
-        if not device:
-            return ('can not find door acs device', 400)
-        acs_control.delay(device.device_index_code, 2)
-        return ('order has been initiated', 200)
+        device_index_code = args.get("device_index_code")
+        room_no = args.get("room_no")
+        if device_index_code:
+            return open_door(args.get("device_index_code"))
+        if room_no:
+            success = []
+            failed = []
+            doors = Door.query.filter(Door.room_no_external.like(args.get("room_no"))).all()
+            for d in doors:
+                ret = open_door(d.door_index_code)
+                if ret['code'] == 0:
+                    success.append(d.door_index_code)
+                else:
+                    failed.append(d.door_index_code)
+            if failed:
+                return {
+                    "code": -1,
+                    "message": ','.join(failed) + 'failed'
+                }
+            else:
+                return {
+                    "code": 0,
+                    "message": "ok"
+                }
+        else:
+            return {
+                "code": -1,
+                "message": "need room_no or device_index_code"
+            }
 
 
 class Acs(Resource):
